11af317819e3fd7526dbf7d64f9a7b8ee4209733
envis10n <envis10n@envis10n.dev>
2021-05-12 17:25:00 -0500

    Add SQLite3 support

--- config.ts
+++ config.ts
@@ -3,6 +3,8 @@ import { config } from "https://deno.land/x/dotenv@v2.0.0/mod.ts";
 const conf = config({ safe: true, allowEmptyValues: true });
 
 export interface XeonConfig {
+  DATABASE_CONNECTOR: "mongodb" | "sqlite3";
+  SQLITE_FILEPATH: string;
   MONGODB_URI: string;
   WEBSOCKET_HOST: string;
   WEBSOCKET_PORT: number;
@@ -13,6 +15,8 @@ export interface XeonConfig {
 }
 
 const _config: XeonConfig = {
+  SQLITE_FILEPATH: conf.SQLITE_FILEPATH,
+  DATABASE_CONNECTOR: conf.DATABASE_CONNECTOR as "mongodb" | "sqlite3",
   MONGODB_URI: conf.MONGODB_URI,
   WEBSOCKET_HOST: conf.WEBSOCKET_HOST,
   WEBSOCKET_PORT: Number(conf.WEBSOCKET_PORT),
--- database.ts
+++ database.ts
@@ -1,12 +1,18 @@
 import config from "./config.ts";
-import { Database, MongoDBConnector } from "https://deno.land/x/denodb/mod.ts";
+import {
+  Database,
+  MongoDBConnector,
+  SQLite3Connector,
+} from "https://deno.land/x/denodb/mod.ts";
 
 import models from "./models/mod.ts";
 
-const connector = new MongoDBConnector({
-  uri: config.MONGODB_URI,
-  database: "project_xeon",
-});
+const connector = config.DATABASE_CONNECTOR == "mongodb"
+  ? new MongoDBConnector({
+    uri: config.MONGODB_URI,
+    database: "project_xeon",
+  })
+  : new SQLite3Connector({ filepath: config.SQLITE_FILEPATH });
 
 const db = new Database({ connector });
 
